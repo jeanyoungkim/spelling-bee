5ca272167489cf13f487c8dec963f0e9fd79347c
Jean Kim <jean.kim@nytimes.com>
2017-05-26 10:34:38 -0400

    new icons, longer auto-validation delay, styles

--- classic/main.js
+++ classic/main.js
@@ -93,7 +93,7 @@ const validateAnswer = () => {
 const isValid = (guess) => (guess.includes(centerLetter) && answers.includes(guess) && !foundWords.includes(guess))
 
 const setUpEventListeners = () => {
-	const waitThenCheck = _.debounce(validateAnswer, 350, {'trailing': true, 'leading': false})
+	const waitThenCheck = _.debounce(validateAnswer, 500, {'trailing': true, 'leading': false})
 	const debouncedShuffle = _.debounce(shuffleTiles, 800, {'leading': true, 'trailing': false})
 	document.addEventListener('click', () => { if (input.innerHTML.length > 4) waitThenCheck() })
 	key.addEventListener('click', e => typeLetter(e, 'highlight'));
